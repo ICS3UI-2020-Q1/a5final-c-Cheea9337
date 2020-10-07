c19d55184bea4c1961e935809444c3124bf629f7
Cheea9337 <cheea9337@wrdsb.ca>
2020-10-07 12:42:41 +0000

    finished a5 final

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author Aidan Cheesmond
  */
 public class Main {
 
@@ -9,7 +10,21 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user for a beginning number
+    System.out.println("What number do you want to begin counting from?");
+    int startNumber = input.nextInt();
+
+    // ask the user for an ending number
+    System.out.println("What number do you want to end counting at?");
+    int endNumber = input.nextInt();
+
+    // create a for loop to count up to the number
+    for(int count = startNumber; count <= endNumber; count = count + 2){
+      System.out.println(count);
+    }
     
   }
 }
